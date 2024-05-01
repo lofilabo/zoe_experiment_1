8fe3d372bad425a0b071c3b912ecd5abefb5824b
lofilabo <richardlofi@gmail.com>
2024-05-01 11:00:58 +0100

    First Redone Upload
    First Upload

---
+++ 02/zoe-master/unacceptable.h
@@ -0,0 +1,2 @@
+#include <stdio.h>
+int unacceptable_basis(int);
---
+++ 03/zoe-master/unacceptable.c
@@ -0,0 +1,8 @@
+#include "unacceptable.h"
+
+
+int unacceptable_basis(int j){
+	j++;
+	printf ("This Is Unacceptable!\n");
+	return j;
+}
