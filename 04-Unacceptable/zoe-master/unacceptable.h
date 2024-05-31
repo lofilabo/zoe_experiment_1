9d829ea154f7d18cc1144e12a0b049f427c1a70f
lofilabo <richardlofi@gmail.com>
2024-05-31 20:50:14 +0100

    extra notes in unacceptable.c
    spaces and letters.

--- 04-Unacceptable/zoe-master/unacceptable.h
+++ 04-Unacceptable/zoe-master/unacceptable.h
@@ -4,7 +4,9 @@ int unacceptable_basis2(int);
 int unacceptable_basis3(int);
 int unacceptable_basis4(int);
 
+/*Prototype.  Do Not Modify*/
 int unacceptable_basisZ(int);
+
 int unacceptable_basisZZ1(int,int);
 int unacceptable_basisZZ2(int,int);
 int unacceptable_basisZZ3(int,int);
