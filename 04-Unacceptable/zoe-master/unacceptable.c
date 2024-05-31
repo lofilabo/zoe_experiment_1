5d61a7376238ceffb0a56e1b24e9e2c100b35cf4
lofilabo <richardlofi@gmail.com>
2024-05-31 15:57:29 +0100

    added #4 - for binding.c Extention practice
    binding.c
    unacceptable.c
    unacceptable.h
    Practice and Practice ^^

---
+++ 04-Unacceptable/zoe-master/unacceptable.c
@@ -0,0 +1,32 @@
+#include "unacceptable.h"
+
+int unacceptable_basis1(int j){
+	j++;
+	printf ("This Is Unacceptable! (No.1 - The very first!!)\n");
+	return j;
+}
+
+
+int unacceptable_basis2(int j){
+	j=123;
+	printf ("Number 2\n");
+	return j;
+}
+
+
+int unacceptable_basis3(int j){
+	j=987;
+	printf ("Number 3\n");
+	return j;
+}
+int unacceptable_basis4(int j){
+	j=000;
+	printf ("Number 4\n");
+	return j;
+}
+
+int unacceptable_basisZ(int j){
+	j=1946;
+	printf ("Number Z\n");
+	return j;
+}
---
+++ 04-Unacceptable/zoe-master/unacceptable.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int unacceptable_basis1(int);
+int unacceptable_basis2(int);
+int unacceptable_basis3(int);
+int unacceptable_basis4(int);
+
+int unacceptable_basisZ(int);
+
