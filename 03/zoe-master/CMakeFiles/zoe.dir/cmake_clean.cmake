35683f4410629a6dece69ab0e1d42c7def1d175b
lofilabo <richardlofi@gmail.com>
2024-06-07 22:00:26 +0100

    ok...
    whatever

---
+++ 03/zoe-master/CMakeFiles/zoe.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zoe.dir/binding.c.o"
+  "CMakeFiles/zoe.dir/error.c.o"
+  "CMakeFiles/zoe.dir/inet.c.o"
+  "CMakeFiles/zoe.dir/interpreter.c.o"
+  "CMakeFiles/zoe.dir/lexer.c.o"
+  "CMakeFiles/zoe.dir/main.c.o"
+  "CMakeFiles/zoe.dir/parser.c.o"
+  "CMakeFiles/zoe.dir/tokenizer.c.o"
+  "CMakeFiles/zoe.dir/unacceptable.c.o"
+  "CMakeFiles/zoe.dir/unicode.c.o"
+  "zoe"
+  "zoe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zoe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
